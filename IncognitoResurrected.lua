878f19bcc92d1b06e069e8c9c4557fe20a31bbc9
Starlynk <info@starlynk.net>
2026-01-11 21:59:02 -0800

    Update TOC

--- IncognitoResurrected.lua
+++ IncognitoResurrected.lua
@@ -1,4 +1,4 @@
---  Version: 1.4.0
+--  Version: 1.4.1
 IncognitoResurrected = LibStub("AceAddon-3.0"):NewAddon("IncognitoResurrected",
                                                         "AceConsole-3.0",
                                                         "AceEvent-3.0",
